099da2a4eb1055f68246011ab9c223a9ebaea1db
Krzysztof Miszke <krzysztofmiszke@gmail.com>
2023-11-13 22:38:15 +0100

    Main logic done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/data.service';
+
+export interface Weather {
+  city: City,
+  cnt: number,
+  cod: string,
+  list: [],
+  message: number;
+}
+
+export interface City {
+  coord: Coordinates,
+  country: string,
+  id: number,
+  name: string,
+  population: number,
+  sunrise: number,
+  sunset: number,
+  timezone: number,
+}
+
+export interface Coordinates {
+  latitude: number,
+  longitude: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'weatherApp';
+  data: any | undefined;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.data = this.dataService.getWeather().subscribe(
+      (data) => {
+        this.data = data;
+        console.log(data)
+      },
+      (error) => {
+        console.error(error, "error xd")
+      }
+    )
+  }
+
+  
+
+
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Weather } from 'src/app/app.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  zabrze: string = '3080985'
+  apiKey: string = 'a77716829359c16e80a756f4d9593c06';
+  apiUrl: string = `http://api.openweathermap.org/data/2.5/forecast?id=${this.zabrze}&appid=`;
+  metric: string = '&units=metric';
+
+  constructor(private http: HttpClient) { }
+
+  getWeather(): Observable<Partial<Weather>> {
+    return this.http.get(`${this.apiUrl}${this.apiKey}${this.metric}`).pipe(
+      map((response: any) => {
+        return response;
+      })
+    )
+  }
+}
