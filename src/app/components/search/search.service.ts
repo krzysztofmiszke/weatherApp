2820a3cbf10e985f74341389b14b0c0780080b4e
Krzysztof Miszke <krzysztofmiszke@gmail.com>
2023-11-15 20:26:52 +0100

    COde refactor

--- src/app/components/search/search.service.ts
+++ src/app/components/search/search.service.ts
@@ -1,22 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Subject, map } from 'rxjs';
-import { Weather } from 'src/app/components/weather-card/weather-card.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
-
+  
   private searchQuerySubject = new Subject<string>();
-
-  location: string = '';
+  
+  baseUrl: string = 'http://api.openweathermap.org/data/2.5/forecast?'
   apiKey: string = 'a77716829359c16e80a756f4d9593c06';
-  apiUrl: string = `http://api.openweathermap.org/data/2.5/forecast?id=${this.location}&appid=`;
   metric: string = '&units=metric';
   count: string = '&cnt=40';
-
-  baseUrl: string = 'http://api.openweathermap.org/data/2.5/forecast?'
+  
+  constructor(private http: HttpClient) { }
 
   setSearchQuery(query: string): void {
     this.searchQuerySubject.next(query);
@@ -26,22 +24,9 @@ export class SearchService {
     return this.searchQuerySubject.asObservable();
   }
 
-  // getWeather1(location: string): Observable<Partial<Weather>> {
-  //   this.location = location;
-  //   return this.http.get(`${this.apiUrl}${this.apiKey}${this.metric}${this.count}`).pipe(
-  //     map((response: any) => {
-  //       return response;
-  //     })
-  //   )
-  // }
-
   getWeather(searchValue: string): Observable<any> {
-    
-    const url = `${this.baseUrl}q=${searchValue}&appid=${this.apiKey}${this.metric}${this.count}`
-    // return this.http.get(url);
-    console.log(url);
+    const url = `${this.baseUrl}q=${searchValue}&appid=${this.apiKey}${this.metric}${this.count}`;
     return this.http.get(url);
   }
 
-  constructor(private http: HttpClient) { }
 }
