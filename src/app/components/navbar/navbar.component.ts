e8210a99621517e57a2914842070ac2542d4f592
Krzysztof Miszke <krzysztofmiszke@gmail.com>
2024-01-16 21:38:49 +0100

    Geolocation done. On safari works better.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,12 +9,31 @@ export class NavbarComponent {
 
   @Output() weatherType = new EventEmitter<boolean>();
 
-  isGeoLocationON: boolean = false;
+  isGeoLocationON: boolean | undefined;
+  checked: string = '';
 
-  ngOnInit(): void {};
+  ngOnInit(): void {
+    this.checkIsGeoLocationON();
+  };
+
+  checkIsGeoLocationON() {
+    const storedToggleValue: any = localStorage.getItem('isGeolocationON'); //string
+    this.isGeoLocationON = JSON.parse(storedToggleValue);
+    this.isGeoLocationON ? this.checked = 'checked' : '';
+    console.log(this.isGeoLocationON)
+  }
+
+  onToggleChange() {
+    
+    // localStorage.setItem('isGeolocationON', JSON.stringify(this.togglePosition))
+    // console.log(localStorage.setItem('isGeolocationON', JSON.stringify(this.togglePosition)));
+  }
 
   changeWeatherType(): void {
     this.isGeoLocationON = !this.isGeoLocationON;
+    const togglePosition = this.isGeoLocationON;
+    localStorage.setItem('isGeolocationON', JSON.stringify(togglePosition))
+    console.log(togglePosition, this.isGeoLocationON)
     this.weatherType.emit(this.isGeoLocationON);
   }
 
